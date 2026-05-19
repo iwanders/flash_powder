301c60146f48b760b99069a1928c9714e77abca9
Ivor Wanders <ivor@iwanders.net>
2026-05-18 21:40:42 -0400

    Save and load test with state dict.

--- flash_powder/src/nn.rs
+++ flash_powder/src/nn.rs
@@ -10,6 +10,35 @@ use torch_stable::StableTorchResult;
 use crate::functional;
 use crate::{Ten, Tensor, core_methods::CoreMethods};
 
+// from https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=f0cee315491dc3c3b6b3f467d6a3b072
+// Provide a custom trait so that we can write a blanket implementation.
+pub trait AsAny {
+    fn as_any_ref(&self) -> &dyn std::any::Any;
+
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any;
+
+    fn as_any_box(self: Box<Self>) -> Box<dyn std::any::Any>;
+}
+
+impl<T> AsAny for T
+where
+    T: std::any::Any,
+{
+    // This cast cannot be written in a default implementation so cannot be
+    // moved to the original trait without implementing it for every type.
+    fn as_any_ref(&self) -> &dyn std::any::Any {
+        self
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+        self
+    }
+
+    fn as_any_box(self: Box<Self>) -> Box<dyn std::any::Any> {
+        self
+    }
+}
+
 #[derive(Debug, Clone)]
 pub enum Data {
     /// Tensors that record gradients, typically weights.
@@ -119,7 +148,7 @@ impl<'a> StateDictAdaptor for NamespacedStateDictAdaptor<'a> {
 /// - Parameters; Tensors that record gradient, typically weights, like `weight` of linear.
 /// - Buffers; Tensor that do not record gradients, typically updated during forward step; `mean`, `variance` of BatchNorm.
 /// - Additionally state, not necessarily tensors, required for implementation or configuration of a Module.
-pub trait Module: std::fmt::Debug + dyn_clone::DynClone {
+pub trait Module: std::fmt::Debug + dyn_clone::DynClone + AsAny {
     fn forward(&self, input: &Ten<'_>) -> Result<Tensor, anyhow::Error>;
     // These look relevant;
     // register_buffer
@@ -145,6 +174,13 @@ pub trait Module: std::fmt::Debug + dyn_clone::DynClone {
         let _ = dict;
         Ok(())
     }
+
+    fn into_boxed(self: Self) -> Box<dyn Module>
+    where
+        Self: Sized + 'static,
+    {
+        Box::new(self)
+    }
 }
 
 dyn_clone::clone_trait_object!(Module);
@@ -152,7 +188,7 @@ dyn_clone::clone_trait_object!(Module);
 /// Sequential module
 ///
 /// - pytorch equivalent; <https://docs.pytorch.org/docs/2.12/generated/torch.nn.Sequential.html>
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct Sequential {
     modules: Vec<Box<dyn Module>>,
 }
@@ -181,6 +217,65 @@ impl Module for Sequential {
             v.load_state_dict(&dict.namespaced(&name))?
         }
         Ok(())
+    }
+}
+impl Sequential {
+    pub fn new() -> Self {
+        Default::default()
+    }
+    pub fn push<T: Into<Box<dyn Module>>>(&mut self, m: T) {
+        self.modules.push(m.into())
+    }
+    pub fn push_boxed(&mut self, m: Box<dyn Module>) {
+        self.modules.push(m)
+    }
+    pub fn get_mut(&mut self, index: usize) -> Option<&mut Box<dyn Module>> {
+        self.modules.get_mut(index)
+    }
+    pub fn get_mut_as<T: Sized + 'static>(&mut self, index: usize) -> Option<&mut T> {
+        use std::ops::DerefMut;
+        if let Some(v) = self.get_mut(index) {
+            v.deref_mut().as_any_mut().downcast_mut()
+        } else {
+            None
+        }
+    }
+    pub fn get(&self, index: usize) -> Option<&Box<dyn Module>> {
+        self.modules.get(index)
+    }
+    pub fn get_as<T: Sized + 'static>(&self, index: usize) -> Option<&T> {
+        use std::ops::Deref;
+        if let Some(v) = self.get(index) {
+            v.deref().as_any_ref().downcast_ref()
+        } else {
+            None
+        }
+    }
+}
+
+use std::iter::FromIterator;
+
+impl<T: Module + 'static> FromIterator<T> for Sequential {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut items = Sequential::new();
+
+        for item in iter {
+            items.push(item.into_boxed());
+        }
+
+        items
+    }
+}
+
+impl FromIterator<Box<dyn Module>> for Sequential {
+    fn from_iter<I: IntoIterator<Item = Box<dyn Module>>>(iter: I) -> Self {
+        let mut items = Sequential::new();
+
+        for item in iter {
+            items.push(item);
+        }
+
+        items
     }
 }
 
@@ -290,6 +385,93 @@ mod test {
 
         let bar = foo.namespaced("bar");
         assert_eq!(bar.tensor("buz").unwrap().as_f64()?, &3.0);
+        Ok(())
+    }
+
+    #[test]
+    fn test_flash_powder_save_load() -> StableTorchResult<()> {
+        let zero: Tensor = (0.0,).try_into()?;
+        let one: Tensor = (1.0,).try_into()?;
+        let two: Tensor = (2.0,).try_into()?;
+        let three: Tensor = (3.0,).try_into()?;
+        let four: Tensor = (4.0,).try_into()?;
+
+        let linear1 = Linear {
+            weight: one,
+            bias: None,
+        };
+        let linear2 = Linear {
+            weight: two.clone(),
+            bias: Some(two.clone()),
+        };
+        let linear3 = Linear {
+            weight: three.clone(),
+            bias: Some(three.clone()),
+        };
+        let seq: Sequential = vec![linear1, linear2.clone()].drain(..).collect();
+        let seq2: Sequential = vec![linear2.into_boxed(), linear3.into_boxed()]
+            .drain(..)
+            .collect();
+
+        let mut seq_root: Sequential = Default::default();
+        seq_root.push_boxed(Box::new(Conv2d {
+            weight: three.clone(),
+            bias: Some(four.clone()),
+            options: Default::default(),
+        }));
+        seq_root.push_boxed(Box::new(seq));
+        seq_root.push_boxed(Box::new(seq2));
+        println!("root: {seq_root:#?}");
+        let seq_root_at_start = format!("{:?}", seq_root);
+
+        let s = seq_root.state_dict()?;
+        println!("s: {s:?}");
+
+        // THis works, but it's hardly a test.
+        seq_root.load_state_dict(&s)?;
+
+        // let z = seq_root.get_mut(0).unwrap();
+        // This is quite the mouth ful :/
+        // let convmut: &mut Conv2d = (*z).deref_mut().as_any_mut().downcast_mut().unwrap();
+        // Okay; this then;
+        {
+            let _: &mut Conv2d = seq_root.get_mut_as::<Conv2d>(0).unwrap();
+            let _: &Conv2d = seq_root.get_as::<Conv2d>(0).unwrap();
+        }
+        // Now that we can get mutable access into the sequential... after it was created, we can zero out its
+        // tensors lol.
+        {
+            let start: &mut Conv2d = seq_root.get_mut_as::<Conv2d>(0).unwrap();
+            start.weight = zero.clone();
+            start.bias = None;
+        }
+
+        let seq1 = seq_root.get_mut_as::<Sequential>(1).unwrap();
+        let l1: &mut Linear = seq1.get_mut_as::<Linear>(0).unwrap();
+        l1.weight = zero.clone();
+        l1.bias = None;
+        let l2: &mut Linear = seq1.get_mut_as::<Linear>(1).unwrap();
+        l2.weight = zero.clone();
+        l2.bias = None;
+
+        let seq2 = seq_root.get_mut_as::<Sequential>(2).unwrap();
+        let l2: &mut Linear = seq2.get_mut_as::<Linear>(0).unwrap();
+        l2.weight = zero.clone();
+        l2.bias = None;
+        let l3: &mut Linear = seq2.get_mut_as::<Linear>(1).unwrap();
+        l3.weight = zero.clone();
+        l3.bias = None;
+
+        // Now that should all be zero'd out.
+        println!("root: {seq_root:?}");
+        assert_ne!(format!("{:?}", seq_root), seq_root_at_start);
+
+        // Now load the state dict.
+        seq_root.load_state_dict(&s)?;
+
+        // Now it should be identical to the start, that confirms we have loaded all tensors again.
+        assert_eq!(format!("{:?}", seq_root), seq_root_at_start);
+
         Ok(())
     }
 }
