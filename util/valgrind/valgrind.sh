fd0ab2c090ee7ca7cd6f80a40822830115e47b85
Ivor Wanders <ivor@iwanders.net>
2026-07-19 14:49:07 -0400

    Add temporary suppression for this leak.
    It seems to be here:
    
    https://github.com/pytorch/pytorch/blob/7a247c9f4626c5bac80d45ed3a6e4c6eeba54941/torch/csrc/stable/stableivalue_conversions.h#L819-L823
    
    New string on the heap, a value of it is returned, but the string is never freed.

--- util/valgrind/valgrind.sh
+++ util/valgrind/valgrind.sh
@@ -5,4 +5,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # Gen suppressions;
 #valgrind -s --gen-suppressions=all --leak-check=full  --suppressions="${DIR}"/combined.supp  --suppressions="${DIR}"/manual.supp --suppressions="${DIR}"/rust_generic.supp $*
 # Run the test:
-valgrind -s  --error-exitcode=1  --leak-check=full  --suppressions="${DIR}"/combined.supp  --suppressions="${DIR}"/manual.supp --suppressions="${DIR}"/rust_generic.supp $*
+valgrind -s  --error-exitcode=1  --leak-check=full  --suppressions="${DIR}"/combined.supp  --suppressions="${DIR}"/manual.supp --suppressions="${DIR}"/rust_generic.supp --suppressions="${DIR}"/temporary.supp $*
