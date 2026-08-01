d5533f64dad7924c20aff72b9254b6455947fc2e
Ivor Wanders <ivor@iwanders.net>
2026-08-01 19:31:36 -0400

    PlusEquals analysis.

--- util/track_kernels.py
+++ util/track_kernels.py
@@ -22,3 +22,25 @@ print(r)
 r[0,0] = 3.0
 print(r)
 print(color_lookup)
+
+
+print("doing the plus assign")
+@torch.compile
+def plus_assign_v(left, right):
+    left += right
+    # heh, this doesn't result in a single kernel, instead;
+    # Topologically Sorted Source Nodes: [left], Original ATen: [aten.add, aten.copy_]
+    #triton_poi_fused_add_copy__0.run(arg0_1, arg1_1, arg0_1, 4, stream=raw_stream0)
+    # Which pretty much does;
+    # tmp0 = load left
+    # tmp1 = load right
+    # tmp2 = tmp0 + tmp1
+    # store(left, tmp2)
+
+
+
+a = torch.tensor([(1.0, 0.0), (0.0, 1.0)])
+b = torch.tensor([(2.0, 1.0), (1.0, 2.0)])
+# Using cuda here ensures we actually see a kernel.
+r = plus_assign_v(a.to("cuda"), b.to("cuda"))
+print(r)
